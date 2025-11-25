f2ecc0ed422994577e521e05b09abddca95fa182
GonzaSai <136342939+GonzaSai@users.noreply.github.com>
2025-11-25 09:20:17 -0300

    kjbbi

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+
+let configuracionFirebase = {
+    apiKey: "AIzaSyDmN4qBz3Aw5x7GO0Z-jB_viG6esOtbFVI",
+    authDomain: "huertili-datini.firebaseapp.com",
+    databaseURL: "https://huertili-datini-default-rtdb.firebaseio.com",
+    projectId: "huertili-datini",
+    storageBucket: "huertili-datini.firebasestorage.app",
+    messagingSenderId: "675544360936",
+    appId: "1:675544360936:web:e50313a4113bf99980265d"
+};
+
+let aplicacion = initializeApp(configuracionFirebase);
+
 let url = "https://huertili-datini-default-rtdb.firebaseio.com/suelo.json";
 
 let elementoTemp = document.querySelector("#temp");
@@ -7,8 +21,8 @@ let elementoEstado = document.querySelector("#estado");
 let obtenerDatos = async function () {
     let respuesta = await fetch(url);
     let datos = await respuesta.json();
-    let temperatura = datos.temperatura;
-    let humedad = datos.humedad;
+    let temperatura = data.temperatura;
+    let humedad = data.humedadAire;
     elementoTemp.textContent = temperatura;
     elementoHum.textContent = humedad;
     let estado = "";
