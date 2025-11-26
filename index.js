ccb9de3af50a6d80865106bd97f80c4f818613ed
jazminrodriguez1234 <136344528+jazminrodriguez1234@users.noreply.github.com>
2025-11-26 08:21:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,4 +22,43 @@ onValue(refDatos, (snapshot) => {
     hum.textContent = `La huerta tiene una temperatura en el suelo de ${huerta.tempSuelo}° y de humedad de ${huerta.humSuelo}`
     temp.textContent = `La huerta tiene una temperatura en el suelo de ${huerta.tempSuelo}° y de humedad de ${huerta.humSuelo}`
     estado.textContent = `La huerta tiene una temperatura en el suelo de ${huerta.tempSuelo}° y de humedad de ${huerta.humSuelo}`
+    
+})
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCrB4xgCBRmpujDcvOOihSVZ6MIFOPE56M",
+    authDomain: "proyectoclase1-2025.firebaseapp.com",
+    databaseURL: "https://proyectoclase1-2025-default-rtdb.firebaseio.com",
+    projectId: "proyectoclase1-2025",
+    storageBucket: "proyectoclase1-2025.firebasestorage.app",
+    messagingSenderId: "822869341917",
+    appId: "1:822869341917:web:8d4d76a6493df78963cd34"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+let parrafo = document.querySelector("p");
+const refDatos = ref(db, "/huerta");
+
+onValue(refDatos, (snapshot) => {
+    console.log(snapshot.val())
+    let huerta = snapshot.val()
+    hum.textContent = ` ${huerta.tempSuelo}°  ${huerta.humSuelo}`
+    temp.textContent = `${huerta.tempSuelo}°  ${huerta.humSuelo}`
+    estado.textContent = ` ${huerta.tempSuelo}°  ${huerta.humSuelo}`
+})
+
+<img id="plantita" src="viva.png" width="150">
+
+
+let humedad = -30; // cambia este valor
+
+if (humedad <= -30) {
+    document.getElementById("plantita").src = "muerta.png";
+} else {
+    document.getElementById("plantita").src = "viva.png";
+}
 })
